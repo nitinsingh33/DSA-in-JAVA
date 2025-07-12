6f35483bc957337cbb6403a2fe94c8683c34ddf3
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-07-12 12:29:50 +0530

    solve switch-case based questions of TUF

---
+++ Basics-01/Switch_case.java
@@ -0,0 +1,46 @@
+/*
+Given the integer day denoting the day number, print on the screen which day of the week it is. Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+
+Ensure only the 1st letter of the answer is capitalised.
+ */
+
+import java.util.Scanner;
+
+public class Switch_case {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int day;
+        System.out.println("Enter the number (1-7): ");
+        day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+        sc.close();
+
+    }
+}
